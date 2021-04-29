89f7a3c294760aaef9c49495a63dfc9f2a79d52d
Rassena <gadzinski.michal@gmail.com>
2021-04-29 08:15:22 +0200

    main

---
+++ src/components/PersonsTable.vue
@@ -0,0 +1,31 @@
+<template>
+ <div id="persons-table">
+ <table>
+ <thead>
+ <tr>
+ <th>Imię i nazwisko</th>
+ <th>email</th>
+ <th>telefon</th>
+ </tr>
+ </thead>
+ <tbody>
+ <tr>
+ <td>Jan Kowalski</td>
+ <td>jan.kowalski@example.pl</td>
+ <td>+48 603-788-987</td>
+ </tr>
+ <tr>
+ <td>Maria Koniuszy</td>
+ <td>m.kon@example.com</td>
+ <td>+48 730-889-966</td>
+ </tr>
+ </tbody>
+ </table>
+ </div>
+</template>
+<script>
+ export default {  
+ name: 'persons-table',
+ }
+</script>
+<style scoped></style>
